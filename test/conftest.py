53cf4a4d393e898366da53ddaa80352e4be50441
cVladu <catalin.vladu@axelera.ai>
2025-03-13 01:28:01 +0200

    Trying to see what happens
    with multiple commits
    that also span
    different
    
    line lengths
    
    whatever
    more whatever
    
    (cherry picked from commit 0f30a4d3f09b4a3d0eb029538a1df07a53fefd58)
    
    # Conflicts:
    #	test/conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 import pytest
 
+<<<<<<< HEAD
+=======
+# Second line added
+# Third
+
+>>>>>>> 0f30a4d (Trying to see what happens)
 zephyr_testcase = pytest.mark.zephyr_testcase
